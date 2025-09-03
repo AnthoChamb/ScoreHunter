d8eb7d473786547f8bf09ee04b1faf7021e068e8
Anthony Chamberland <anthochamberland19@outlook.com>
2025-09-02 22:46:21 -0400

    Break Write tablature into smaller private methods

--- src/ScoreHunter.Drawing.Svg/IO/SvgTablatureWriter.cs
+++ src/ScoreHunter.Drawing.Svg/IO/SvgTablatureWriter.cs
@@ -63,69 +63,8 @@ namespace ScoreHunter.Drawing.Svg.IO
             _writer.WriteAttributeString(null, "width", null, (StaffPaddingX * 2 + TicksToPixels(tablature.TicksPerStaff)).ToString(CultureInfo.InvariantCulture));
             _writer.WriteAttributeString(null, "height", null, (StaffPaddingY * 2 + ((StaffHeight + StaffPaddingY) * tablature.Staves.Count())).ToString(CultureInfo.InvariantCulture));
 
-            _writer.WriteStartElement("style");
-            _writer.WriteString("text { font-family: sans-serif; }");
-            _writer.WriteString(".s, .w { fill: transparent; stroke-width: 1px; }");
-            _writer.WriteString(".s { stroke: black; }");
-            _writer.WriteString(".w { stroke: gray; }");
-            _writer.WriteString(".m, .t { font-size: " + XmlConvert.ToString(TextFontSize) + "px; }");
-            _writer.WriteString(".n { font-size: " + XmlConvert.ToString(StaffHeight / 2) + "px; }");
-            _writer.WriteString(".m { fill: red; }");
-            _writer.WriteString(".t, .n, .l { fill: gray; }");
-            _writer.WriteString(".g, .y, .b { opacity: 0.3; }");
-            _writer.WriteString(".g { fill: green; }");
-            _writer.WriteString(".y { fill: yellow; }");
-            _writer.WriteString(".b { fill: blue; }");
-            _writer.WriteEndElement();
-
-            _writer.WriteStartElement("defs");
-
-            _writer.WriteStartElement("path");
-            _writer.WriteAttributeString("id", "h");
-            WriteStartAttributePathCommand();
-            WriteMoveTo(0, 0);
-            WriteVerticalLineToRelative(StaffHeight);
-            WriteEndAttributePathCommand();
-            _writer.WriteAttributeDouble("stroke-width", 1);
-            _writer.WriteEndElement();
-
-            _writer.WriteStartElementUse("#h");
-            _writer.WriteAttributeString("id", "s");
-            _writer.WriteAttributeString("stroke", "black");
-            _writer.WriteEndElement();
-
-            _writer.WriteStartElementUse("#h");
-            _writer.WriteAttributeString("id", "l");
-            _writer.WriteAttributeString("stroke", "gray");
-            _writer.WriteEndElement();
-
-            _writer.WriteStartElement("circle");
-            _writer.WriteAttributeString("id", "n");
-            _writer.WriteAttributeDouble("r", NoteSize / 2);
-            _writer.WriteAttributeString("stroke", "black");
-            _writer.WriteAttributeDouble("stroke-width", 1);
-            _writer.WriteEndElement();
-
-            _writer.WriteStartElementUse("#n");
-            _writer.WriteAttributeString("id", "b");
-            _writer.WriteAttributeString("fill", "black");
-            _writer.WriteEndElement();
-
-            _writer.WriteStartElementUse("#n");
-            _writer.WriteAttributeString("id", "w");
-            _writer.WriteAttributeString("fill", "white");
-            _writer.WriteEndElement();
-
-            _writer.WriteStartElement("rect");
-            _writer.WriteAttributeString("id", "o");
-            _writer.WriteAttributeDouble("width", NoteSize / 2);
-            _writer.WriteAttributeDouble("height", StaffHeight + NoteSize);
-            _writer.WriteAttributeString("stroke", "black");
-            _writer.WriteAttributeString("fill", "white");
-            _writer.WriteAttributeDouble("stroke-width", 1);
-            _writer.WriteEndElement();
-
-            _writer.WriteEndElement();
+            WriteStyleElement();
+            WriteDefsElement();
 
             ITimeSignature currentTimeSignature = null;
             var measureCount = 0;
@@ -137,24 +76,7 @@ namespace ScoreHunter.Drawing.Svg.IO
                 var measureCountY = staffY - (NoteSize / 2 + TextPaddingY);
                 var tempoY = measureCountY - (TextFontSize + TextPaddingY);
 
-                _writer.WriteStartElement("path");
-                WriteStartAttributePathCommand();
-                WriteMoveTo(StaffPaddingX, staffY);
-                WriteHorizontalLineToRelative(staffWidth);
-                WriteVerticalLineToRelative(StaffHeight);
-                WriteHorizontalLineToRelative(-staffWidth);
-                WriteEndAttributePathCommand();
-                _writer.WriteAttributeString("class", "s");
-                _writer.WriteEndElement();
-
-                _writer.WriteStartElement("path");
-                WriteStartAttributePathCommand();
-                WriteMoveTo(StaffPaddingX, staffY + StaffHeight / 2);
-                WriteHorizontalLineToRelative(staffWidth);
-                WriteEndAttributePathCommand();
-                _writer.WriteAttributeString("class", "w");
-                _writer.WriteEndElement();
-
+                WriteStaff(staffY, staffWidth);
 
                 foreach (var measure in staff.Measures)
                 {
@@ -167,23 +89,7 @@ namespace ScoreHunter.Drawing.Svg.IO
                     {
                         currentTimeSignature = measure.TimeSignature;
                         var timeSignatureX = measureX + TicksToPixels(currentTimeSignature.Ticks - measure.StartTicks) + TextFontSize;
-
-                        _writer.WriteStartElement("text");
-                        _writer.WriteAttributeDouble("x", timeSignatureX);
-                        _writer.WriteAttributeDouble("y", staffY + StaffHeight / 2 - TextPaddingY / 2);
-                        _writer.WriteAttributeString("class", "n");
-
-                        _writer.WriteValue(currentTimeSignature.Numerator);
-
-                        _writer.WriteEndElement();
-                        _writer.WriteStartElement("text");
-                        _writer.WriteAttributeDouble("x", timeSignatureX);
-                        _writer.WriteAttributeDouble("y", staffY + StaffHeight - TextPaddingY / 2);
-                        _writer.WriteAttributeString("class", "n");
-
-                        _writer.WriteValue(currentTimeSignature.Denominator);
-
-                        _writer.WriteEndElement();
+                        WriteTimeSignature(timeSignatureX, staffY, currentTimeSignature);
                     }
 
                     foreach (var beat in measure.Beats)
@@ -200,36 +106,23 @@ namespace ScoreHunter.Drawing.Svg.IO
                     var sustainX = StaffPaddingX + TicksToPixels(sustain.StartTicks - staff.StartTicks);
                     var sustainWidth = TicksToPixels(sustain.EndTicks - sustain.StartTicks);
 
-                    void WriteSustain(double y)
-                    {
-                        _writer.WriteStartElement("path");
-                        WriteStartAttributePathCommand();
-                        WriteMoveTo(sustainX, y);
-                        WriteHorizontalLineToRelative(sustainWidth);
-                        WriteVerticalLineToRelative(SustainHeight);
-                        WriteHorizontalLineToRelative(-sustainWidth);
-                        WriteEndAttributePathCommand();
-                        _writer.WriteAttributeString("class", "l");
-                        _writer.WriteEndElement();
-                    }
-
                     switch (sustain.Frets.Flags)
                     {
                         case FretFlags.Black1:
                         case FretFlags.White1:
                         case FretFlags.Black1 | FretFlags.White1:
-                            WriteSustain(staffY - SustainHeight / 2);
+                            WriteSustain(sustainX, staffY - SustainHeight / 2, sustainWidth);
                             break;
                         case FretFlags.Open:
                         case FretFlags.Black2:
                         case FretFlags.White2:
                         case FretFlags.Black2 | FretFlags.White2:
-                            WriteSustain(staffY + StaffHeight / 2 - SustainHeight / 2);
+                            WriteSustain(sustainX, staffY + StaffHeight / 2 - SustainHeight / 2, sustainWidth);
                             break;
                         case FretFlags.Black3:
                         case FretFlags.White3:
                         case FretFlags.Black3 | FretFlags.White3:
-                            WriteSustain(staffY + StaffHeight - SustainHeight / 2);
+                            WriteSustain(sustainX, staffY + StaffHeight - SustainHeight / 2, sustainWidth);
                             break;
                     }
                 }
@@ -238,28 +131,12 @@ namespace ScoreHunter.Drawing.Svg.IO
                 {
                     measureCount++;
                     var measureX = StaffPaddingX + TicksToPixels(measure.StartTicks - staff.StartTicks);
-
-                    _writer.WriteStartElement("text");
-                    _writer.WriteAttributeDouble("x", measureX);
-                    _writer.WriteAttributeDouble("y", measureCountY);
-                    _writer.WriteAttributeString("class", "m");
-
-                    _writer.WriteValue(measureCount);
-
-                    _writer.WriteEndElement();
+                    WriteMeasureCount(measureX, measureCountY, measureCount);
 
                     foreach (var tempo in measure.Tempos)
                     {
                         var tempoX = measureX + TicksToPixels(tempo.Ticks - measure.StartTicks);
-
-                        _writer.WriteStartElement("text");
-                        _writer.WriteAttributeDouble("x", tempoX);
-                        _writer.WriteAttributeDouble("y", tempoY);
-                        _writer.WriteAttributeString("class", "t");
-
-                        _writer.WriteString("\u2669=" + Math.Round(tempo.BeatsPerMinute));
-
-                        _writer.WriteEndElement();
+                        WriteTempo(tempoX, tempoY, tempo);
                     }
 
                     foreach (var note in measure.Notes)
@@ -367,51 +244,21 @@ namespace ScoreHunter.Drawing.Svg.IO
                 {
                     var heroPowerPhraseX = StaffPaddingX + TicksToPixels(heroPowerPhrase.StartTicks - staff.StartTicks);
                     var heroPowerPhraseWidth = TicksToPixels(heroPowerPhrase.EndTicks - heroPowerPhrase.StartTicks);
-
-                    _writer.WriteStartElement("path");
-                    WriteStartAttributePathCommand();
-                    WriteMoveTo(heroPowerPhraseX, staffY);
-                    WriteHorizontalLineToRelative(heroPowerPhraseWidth);
-                    WriteVerticalLineToRelative(StaffHeight);
-                    WriteHorizontalLineToRelative(-heroPowerPhraseWidth);
-                    WriteClosePath();
-                    WriteEndAttributePathCommand();
-                    _writer.WriteAttributeString("class", "g");
-                    _writer.WriteEndElement();
+                    WritePhrase(heroPowerPhraseX, staffY, heroPowerPhraseWidth, "g");
                 }
 
                 foreach (var highwayPhrase in staff.HighwayPhrases)
                 {
                     var highwayPhraseX = StaffPaddingX + TicksToPixels(highwayPhrase.StartTicks - staff.StartTicks);
                     var highwayPhraseWidth = TicksToPixels(highwayPhrase.EndTicks - highwayPhrase.StartTicks);
-
-                    _writer.WriteStartElement("path");
-                    WriteStartAttributePathCommand();
-                    WriteMoveTo(highwayPhraseX, staffY);
-                    WriteHorizontalLineToRelative(highwayPhraseWidth);
-                    WriteVerticalLineToRelative(StaffHeight);
-                    WriteHorizontalLineToRelative(-highwayPhraseWidth);
-                    WriteClosePath();
-                    WriteEndAttributePathCommand();
-                    _writer.WriteAttributeString("class", "y");
-                    _writer.WriteEndElement();
+                    WritePhrase(highwayPhraseX, staffY, highwayPhraseWidth, "y");
                 }
 
                 foreach (var activation in staff.Activations)
                 {
                     var activationX = StaffPaddingX + TicksToPixels(activation.StartTicks - staff.StartTicks);
                     var activationWidth = TicksToPixels(activation.EndTicks - activation.StartTicks);
-
-                    _writer.WriteStartElement("path");
-                    WriteStartAttributePathCommand();
-                    WriteMoveTo(activationX, staffY);
-                    WriteHorizontalLineToRelative(activationWidth);
-                    WriteVerticalLineToRelative(StaffHeight);
-                    WriteHorizontalLineToRelative(-activationWidth);
-                    WriteClosePath();
-                    WriteEndAttributePathCommand();
-                    _writer.WriteAttributeString("class", "b");
-                    _writer.WriteEndElement();
+                    WritePhrase(activationX, staffY, activationWidth, "b");
                 }
 
                 staffY += StaffHeight + StaffPaddingY;
@@ -424,6 +271,174 @@ namespace ScoreHunter.Drawing.Svg.IO
         {
             Write(tablature);
             return Task.CompletedTask;
+        }
+
+        private void WriteStyleElement()
+        {
+            _writer.WriteStartElement("style");
+            _writer.WriteString("text { font-family: sans-serif; }");
+            _writer.WriteString(".s, .w { fill: transparent; stroke-width: 1px; }");
+            _writer.WriteString(".s { stroke: black; }");
+            _writer.WriteString(".w { stroke: gray; }");
+            _writer.WriteString(".m, .t { font-size: ");
+            _writer.WriteValue(TextFontSize);
+            _writer.WriteString("px; }");
+            _writer.WriteString(".n { font-size: ");
+            _writer.WriteValue(StaffHeight / 2);
+            _writer.WriteString("px; }");
+            _writer.WriteString(".m { fill: red; }");
+            _writer.WriteString(".t, .n, .l { fill: gray; }");
+            _writer.WriteString(".g, .y, .b { opacity: 0.3; }");
+            _writer.WriteString(".g { fill: green; }");
+            _writer.WriteString(".y { fill: yellow; }");
+            _writer.WriteString(".b { fill: blue; }");
+            _writer.WriteEndElement();
+        }
+
+        private void WriteDefsElement()
+        {
+            _writer.WriteStartElement("defs");
+
+            _writer.WriteStartElement("path");
+            _writer.WriteAttributeString("id", "h");
+            WriteStartAttributePathCommand();
+            WriteMoveTo(0, 0);
+            WriteVerticalLineToRelative(StaffHeight);
+            WriteEndAttributePathCommand();
+            _writer.WriteAttributeDouble("stroke-width", 1);
+            _writer.WriteEndElement();
+
+            _writer.WriteStartElementUse("#h");
+            _writer.WriteAttributeString("id", "s");
+            _writer.WriteAttributeString("stroke", "black");
+            _writer.WriteEndElement();
+
+            _writer.WriteStartElementUse("#h");
+            _writer.WriteAttributeString("id", "l");
+            _writer.WriteAttributeString("stroke", "gray");
+            _writer.WriteEndElement();
+
+            _writer.WriteStartElement("circle");
+            _writer.WriteAttributeString("id", "n");
+            _writer.WriteAttributeDouble("r", NoteSize / 2);
+            _writer.WriteAttributeString("stroke", "black");
+            _writer.WriteAttributeDouble("stroke-width", 1);
+            _writer.WriteEndElement();
+
+            _writer.WriteStartElementUse("#n");
+            _writer.WriteAttributeString("id", "b");
+            _writer.WriteAttributeString("fill", "black");
+            _writer.WriteEndElement();
+
+            _writer.WriteStartElementUse("#n");
+            _writer.WriteAttributeString("id", "w");
+            _writer.WriteAttributeString("fill", "white");
+            _writer.WriteEndElement();
+
+            _writer.WriteStartElement("rect");
+            _writer.WriteAttributeString("id", "o");
+            _writer.WriteAttributeDouble("width", NoteSize / 2);
+            _writer.WriteAttributeDouble("height", StaffHeight + NoteSize);
+            _writer.WriteAttributeString("stroke", "black");
+            _writer.WriteAttributeString("fill", "white");
+            _writer.WriteAttributeDouble("stroke-width", 1);
+            _writer.WriteEndElement();
+
+            _writer.WriteEndElement();
+        }
+
+        private void WriteStaff(double y, double width)
+        {
+            _writer.WriteStartElement("path");
+            WriteStartAttributePathCommand();
+            WriteMoveTo(StaffPaddingX, y);
+            WriteHorizontalLineToRelative(width);
+            WriteVerticalLineToRelative(StaffHeight);
+            WriteHorizontalLineToRelative(-width);
+            WriteEndAttributePathCommand();
+            _writer.WriteAttributeString("class", "s");
+            _writer.WriteEndElement();
+
+            _writer.WriteStartElement("path");
+            WriteStartAttributePathCommand();
+            WriteMoveTo(StaffPaddingX, y + StaffHeight / 2);
+            WriteHorizontalLineToRelative(width);
+            WriteEndAttributePathCommand();
+            _writer.WriteAttributeString("class", "w");
+            _writer.WriteEndElement();
+        }
+
+        private void WriteTimeSignature(double x, double y, ITimeSignature timeSignature)
+        {
+            _writer.WriteStartElement("text");
+            _writer.WriteAttributeDouble("x", x);
+            _writer.WriteAttributeDouble("y", y + StaffHeight / 2 - TextPaddingY / 2);
+            _writer.WriteAttributeString("class", "n");
+
+            _writer.WriteValue(timeSignature.Numerator);
+
+            _writer.WriteEndElement();
+
+            _writer.WriteStartElement("text");
+            _writer.WriteAttributeDouble("x", x);
+            _writer.WriteAttributeDouble("y", y + StaffHeight - TextPaddingY / 2);
+            _writer.WriteAttributeString("class", "n");
+
+            _writer.WriteValue(timeSignature.Denominator);
+
+            _writer.WriteEndElement();
+        }
+
+        private void WriteSustain(double x, double y, double width)
+        {
+            _writer.WriteStartElement("path");
+            WriteStartAttributePathCommand();
+            WriteMoveTo(x, y);
+            WriteHorizontalLineToRelative(width);
+            WriteVerticalLineToRelative(SustainHeight);
+            WriteHorizontalLineToRelative(-width);
+            WriteEndAttributePathCommand();
+            _writer.WriteAttributeString("class", "l");
+            _writer.WriteEndElement();
+        }
+
+        private void WriteMeasureCount(double x, double y, int measureCount)
+        {
+            _writer.WriteStartElement("text");
+            _writer.WriteAttributeDouble("x", x);
+            _writer.WriteAttributeDouble("y", y);
+            _writer.WriteAttributeString("class", "m");
+
+            _writer.WriteValue(measureCount);
+
+            _writer.WriteEndElement();
+        }
+
+        private void WriteTempo(double x, double y, ITempo tempo)
+        {
+            _writer.WriteStartElement("text");
+            _writer.WriteAttributeDouble("x", x);
+            _writer.WriteAttributeDouble("y", y);
+            _writer.WriteAttributeString("class", "t");
+
+            _writer.WriteString("\u2669=");
+            _writer.WriteValue(Math.Round(tempo.BeatsPerMinute));
+
+            _writer.WriteEndElement();
+        }
+
+        private void WritePhrase(double x, double y, double width, string @class)
+        {
+            _writer.WriteStartElement("path");
+            WriteStartAttributePathCommand();
+            WriteMoveTo(x, y);
+            WriteHorizontalLineToRelative(width);
+            WriteVerticalLineToRelative(StaffHeight);
+            WriteHorizontalLineToRelative(-width);
+            WriteClosePath();
+            WriteEndAttributePathCommand();
+            _writer.WriteAttributeString("class", @class);
+            _writer.WriteEndElement();
         }
 
         private void WriteStartAttributePathCommand()
