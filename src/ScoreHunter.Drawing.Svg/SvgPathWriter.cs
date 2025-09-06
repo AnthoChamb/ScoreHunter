b692bb8012e5d9867733dda4143b2f0e75f6c106
Anthony Chamberland <anthochamberland19@outlook.com>
2025-09-06 12:43:22 -0400

    Add command async methods

--- src/ScoreHunter.Drawing.Svg/SvgPathWriter.cs
+++ src/ScoreHunter.Drawing.Svg/SvgPathWriter.cs
@@ -177,11 +177,25 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(y);
         }
 
+        public async Task WriteMoveToAsync(double x, double y)
+        {
+            await WriteCommandAsync('M').ConfigureAwait(false);
+            await WriteParameterAsync(x).ConfigureAwait(false);
+            await WriteParameterAsync(y).ConfigureAwait(false);
+        }
+
         public void WriteMoveToRelative(double dx, double dy)
         {
             WriteCommand('m');
             WriteParameter(dx);
             WriteParameter(dy);
+        }
+
+        public async Task WriteMoveToRelativeAsync(double dx, double dy)
+        {
+            await WriteCommandAsync('m').ConfigureAwait(false);
+            await WriteParameterAsync(dx).ConfigureAwait(false);
+            await WriteParameterAsync(dy).ConfigureAwait(false);
         }
 
         public void WriteLineTo(double x, double y)
@@ -191,11 +205,25 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(y);
         }
 
+        public async Task WriteLineToAsync(double x, double y)
+        {
+            await WriteCommandAsync('L').ConfigureAwait(false);
+            await WriteParameterAsync(x).ConfigureAwait(false);
+            await WriteParameterAsync(y).ConfigureAwait(false);
+        }
+
         public void WriteLineToRelative(double dx, double dy)
         {
             WriteCommand('l');
             WriteParameter(dx);
             WriteParameter(dy);
+        }
+
+        public async Task WriteLineToRelativeAsync(double dx, double dy)
+        {
+            await WriteCommandAsync('l').ConfigureAwait(false);
+            await WriteParameterAsync(dx).ConfigureAwait(false);
+            await WriteParameterAsync(dy).ConfigureAwait(false);
         }
 
         public void WriteHorizontalLineTo(double x)
@@ -204,10 +232,22 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(x);
         }
 
+        public async Task WriteHorizontalLineToAsync(double x)
+        {
+            await WriteCommandAsync('H').ConfigureAwait(false);
+            await WriteParameterAsync(x).ConfigureAwait(false);
+        }
+
         public void WriteHorizontalLineToRelative(double dx)
         {
             WriteCommand('h');
             WriteParameter(dx);
+        }
+
+        public async Task WriteHorizontalLineToRelativeAsync(double dx)
+        {
+            await WriteCommandAsync('h').ConfigureAwait(false);
+            await WriteParameterAsync(dx).ConfigureAwait(false);
         }
 
         public void WriteVerticalLineTo(double y)
@@ -216,10 +256,22 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(y);
         }
 
+        public async Task WriteVerticalLineToAsync(double y)
+        {
+            await WriteCommandAsync('V').ConfigureAwait(false);
+            await WriteParameterAsync(y).ConfigureAwait(false);
+        }
+
         public void WriteVerticalLineToRelative(double dy)
         {
             WriteCommand('v');
             WriteParameter(dy);
+        }
+
+        public async Task WriteVerticalLineToRelativeAsync(double dy)
+        {
+            await WriteCommandAsync('v').ConfigureAwait(false);
+            await WriteParameterAsync(dy).ConfigureAwait(false);
         }
 
         public void WriteCubicBezierCurve(double x1, double y1, double x2, double y2, double x, double y)
@@ -233,6 +285,17 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(y);
         }
 
+        public async Task WriteCubicBezierCurveAsync(double x1, double y1, double x2, double y2, double x, double y)
+        {
+            await WriteCommandAsync('C').ConfigureAwait(false);
+            await WriteParameterAsync(x1).ConfigureAwait(false);
+            await WriteParameterAsync(y1).ConfigureAwait(false);
+            await WriteParameterAsync(x2).ConfigureAwait(false);
+            await WriteParameterAsync(y2).ConfigureAwait(false);
+            await WriteParameterAsync(x).ConfigureAwait(false);
+            await WriteParameterAsync(y).ConfigureAwait(false);
+        }
+
         public void WriteCubicBezierCurveRelative(double dx1, double dy1, double dx2, double dy2, double dx, double dy)
         {
             WriteCommand('c');
@@ -244,6 +307,17 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(dy);
         }
 
+        public async Task WriteCubicBezierCurveRelativeAsync(double dx1, double dy1, double dx2, double dy2, double dx, double dy)
+        {
+            await WriteCommandAsync('c').ConfigureAwait(false);
+            await WriteParameterAsync(dx1).ConfigureAwait(false);
+            await WriteParameterAsync(dy1).ConfigureAwait(false);
+            await WriteParameterAsync(dx2).ConfigureAwait(false);
+            await WriteParameterAsync(dy2).ConfigureAwait(false);
+            await WriteParameterAsync(dx).ConfigureAwait(false);
+            await WriteParameterAsync(dy).ConfigureAwait(false);
+        }
+
         public void WriteSmoothCubicBezierCurve(double x2, double y2, double x, double y)
         {
             WriteCommand('S');
@@ -251,6 +325,15 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(y2);
             WriteParameter(x);
             WriteParameter(y);
+        }
+
+        public async Task WriteSmoothCubicBezierCurveAsync(double x2, double y2, double x, double y)
+        {
+            await WriteCommandAsync('S').ConfigureAwait(false);
+            await WriteParameterAsync(x2).ConfigureAwait(false);
+            await WriteParameterAsync(y2).ConfigureAwait(false);
+            await WriteParameterAsync(x).ConfigureAwait(false);
+            await WriteParameterAsync(y).ConfigureAwait(false);
         }
 
         public void WriteSmoothCubicBezierCurveRelative(double dx2, double dy2, double dx, double dy)
@@ -262,6 +345,15 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(dy);
         }
 
+        public async Task WriteSmoothCubicBezierCurveRelativeAsync(double dx2, double dy2, double dx, double dy)
+        {
+            await WriteCommandAsync('s').ConfigureAwait(false);
+            await WriteParameterAsync(dx2).ConfigureAwait(false);
+            await WriteParameterAsync(dy2).ConfigureAwait(false);
+            await WriteParameterAsync(dx).ConfigureAwait(false);
+            await WriteParameterAsync(dy).ConfigureAwait(false);
+        }
+
         public void WriteQuadraticBezierCurve(double x1, double y1, double x, double y)
         {
             WriteCommand('Q');
@@ -269,6 +361,15 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(y1);
             WriteParameter(x);
             WriteParameter(y);
+        }
+
+        public async Task WriteQuadraticBezierCurveAsync(double x1, double y1, double x, double y)
+        {
+            await WriteCommandAsync('Q').ConfigureAwait(false);
+            await WriteParameterAsync(x1).ConfigureAwait(false);
+            await WriteParameterAsync(y1).ConfigureAwait(false);
+            await WriteParameterAsync(x).ConfigureAwait(false);
+            await WriteParameterAsync(y).ConfigureAwait(false);
         }
 
         public void WriteQuadraticBezierCurveRelative(double dx1, double dy1, double dx, double dy)
@@ -280,6 +381,15 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(dy);
         }
 
+        public async Task WriteQuadraticBezierCurveRelativeAsync(double dx1, double dy1, double dx, double dy)
+        {
+            await WriteCommandAsync('q').ConfigureAwait(false);
+            await WriteParameterAsync(dx1).ConfigureAwait(false);
+            await WriteParameterAsync(dy1).ConfigureAwait(false);
+            await WriteParameterAsync(dx).ConfigureAwait(false);
+            await WriteParameterAsync(dy).ConfigureAwait(false);
+        }
+
         public void WriteSmoothQuadraticBezierCurve(double x, double y)
         {
             WriteCommand('T');
@@ -287,11 +397,25 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(y);
         }
 
+        public async Task WriteSmoothQuadraticBezierCurveAsync(double x, double y)
+        {
+            await WriteCommandAsync('T').ConfigureAwait(false);
+            await WriteParameterAsync(x).ConfigureAwait(false);
+            await WriteParameterAsync(y).ConfigureAwait(false);
+        }
+
         public void WriteSmoothQuadraticBezierCurveRelative(double dx, double dy)
         {
             WriteCommand('t');
             WriteParameter(dx);
             WriteParameter(dy);
+        }
+
+        public async Task WriteSmoothQuadraticBezierCurveRelativeAsync(double dx, double dy)
+        {
+            await WriteCommandAsync('t').ConfigureAwait(false);
+            await WriteParameterAsync(dx).ConfigureAwait(false);
+            await WriteParameterAsync(dy).ConfigureAwait(false);
         }
 
         public void WriteArc(double rx, double ry, double angle, bool largeArc, bool sweep, double x, double y)
@@ -306,6 +430,18 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(y);
         }
 
+        public async Task WriteArcAsync(double rx, double ry, double angle, bool largeArc, bool sweep, double x, double y)
+        {
+            await WriteCommandAsync('A').ConfigureAwait(false);
+            await WriteParameterAsync(rx).ConfigureAwait(false);
+            await WriteParameterAsync(ry).ConfigureAwait(false);
+            await WriteParameterAsync(angle).ConfigureAwait(false);
+            await WriteParameterAsync(largeArc).ConfigureAwait(false);
+            await WriteParameterAsync(sweep).ConfigureAwait(false);
+            await WriteParameterAsync(x).ConfigureAwait(false);
+            await WriteParameterAsync(y).ConfigureAwait(false);
+        }
+
         public void WriteArcRelative(double rx, double ry, double angle, bool largeArc, bool sweep, double dx, double dy)
         {
             WriteCommand('a');
@@ -318,9 +454,26 @@ namespace ScoreHunter.Drawing.Svg
             WriteParameter(dy);
         }
 
+        public async Task WriteArcRelativeAsync(double rx, double ry, double angle, bool largeArc, bool sweep, double dx, double dy)
+        {
+            await WriteCommandAsync('a').ConfigureAwait(false);
+            await WriteParameterAsync(rx).ConfigureAwait(false);
+            await WriteParameterAsync(ry).ConfigureAwait(false);
+            await WriteParameterAsync(angle).ConfigureAwait(false);
+            await WriteParameterAsync(largeArc).ConfigureAwait(false);
+            await WriteParameterAsync(sweep).ConfigureAwait(false);
+            await WriteParameterAsync(dx).ConfigureAwait(false);
+            await WriteParameterAsync(dy).ConfigureAwait(false);
+        }
+
         public void WriteClosePath()
         {
             WriteCommand('Z');
+        }
+
+        public async Task WriteClosePathAsync()
+        {
+            await WriteCommandAsync('Z').ConfigureAwait(false);
         }
 
         public virtual void Dispose()
