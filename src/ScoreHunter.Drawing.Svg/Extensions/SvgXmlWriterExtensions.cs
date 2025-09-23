bebe2836c462cbae68917e1f566551e5db33314c
Anthony Chamberland <61058826+AnthoChamb@users.noreply.github.com>
2025-09-22 21:22:36 -0400

    Add SvgXmlWriterExtensions

---
+++ src/ScoreHunter.Drawing.Svg/Extensions/SvgXmlWriterExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using System.Xml;
+
+namespace ScoreHunter.Drawing.Svg.Extensions
+{
+    public static class SvgXmlWriterExtensions
+    {
+        public static void WriteStartElementUse(this XmlWriter writer, string href)
+        {
+            writer.WriteStartElement("use");
+            writer.WriteAttributeString("href", href);
+        }
+
+        public static async Task WriteStartElementUseAsync(this XmlWriter writer, string href)
+        {
+            await writer.WriteStartElementAsync("use").ConfigureAwait(false);
+            await writer.WriteAttributeStringAsync("href", href).ConfigureAwait(false);
+        }
+
+        public static void WriteStartElementUse(this XmlWriter writer, string href, double x, double y)
+        {
+            writer.WriteStartElementUse(href);
+            writer.WriteAttributeDouble("x", x);
+            writer.WriteAttributeDouble("y", y);
+        }
+
+        public static async Task WriteStartElementUseAsync(this XmlWriter writer, string href, double x, double y)
+        {
+            await writer.WriteStartElementUseAsync(href).ConfigureAwait(false);
+            await writer.WriteAttributeDoubleAsync("x", x).ConfigureAwait(false);
+            await writer.WriteAttributeDoubleAsync("y", y).ConfigureAwait(false);
+        }
+    }
+}
--- src/ScoreHunter.Drawing.Svg/Extensions/XmlWriterExtensions.cs
+++ src/ScoreHunter.Drawing.Svg/Extensions/XmlWriterExtensions.cs
@@ -16,40 +16,5 @@ namespace ScoreHunter.Drawing.Svg.Extensions
         {
             await writer.WriteAttributeStringAsync(localName, XmlConvert.ToString(value)).ConfigureAwait(false);
         }
-
-        public static void WriteStartElementRect(this XmlWriter writer, double x, double y, double width, double height)
-        {
-            writer.WriteStartElement("rect");
-            writer.WriteAttributeDouble("x", x);
-            writer.WriteAttributeDouble("y", y);
-            writer.WriteAttributeDouble("width", width);
-            writer.WriteAttributeDouble("height", height);
-        }
-
-        public static void WriteStartElementUse(this XmlWriter writer, string href)
-        {
-            writer.WriteStartElement("use");
-            writer.WriteAttributeString("href", href);
-        }
-
-        public static async Task WriteStartElementUseAsync(this XmlWriter writer, string href)
-        {
-            await writer.WriteStartElementAsync("use").ConfigureAwait(false);
-            await writer.WriteAttributeStringAsync("href", href).ConfigureAwait(false);
-        }
-
-        public static void WriteStartElementUse(this XmlWriter writer, string href, double x, double y)
-        {
-            writer.WriteStartElementUse(href);
-            writer.WriteAttributeDouble("x", x);
-            writer.WriteAttributeDouble("y", y);
-        }
-
-        public static async Task WriteStartElementUseAsync(this XmlWriter writer, string href, double x, double y)
-        {
-            await writer.WriteStartElementUseAsync(href).ConfigureAwait(false);
-            await writer.WriteAttributeDoubleAsync("x", x).ConfigureAwait(false);
-            await writer.WriteAttributeDoubleAsync("y", y).ConfigureAwait(false);
-        }
     }
 }
